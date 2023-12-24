4414ac5a74ab594b0560b310a0629814d3119c99
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2023-12-23 20:09:35 -0800

    Fix route alias

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'exercises' => 'exercise#index'
+  get 'blogs' => 'blog#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
