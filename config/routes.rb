6e86cdb63940b74aabb954f9b8337e62a7bbeed3
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2023-12-30 21:22:53 -0800

    new and show views, routes still need troubleshooting.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   # resources :blogs
   root 'blogs#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #Index route 
   get 'blogs' => 'blog#index', as: 'blogs'
+  #Create route 
   post 'blogs' => 'blog#create' 
+  #New route  
   get 'blogs/new' => 'blog#new', as: 'new_blog'
+  #Show route 
   get 'blogs/:id' => 'blog#show', as: 'blog'
+  #Delete route 
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
