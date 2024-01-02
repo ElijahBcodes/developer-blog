66c55af6d59501ec6c9ed93e014a70cd2275e0bf
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-01-01 18:57:08 -0800

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   get 'blog_posts' => 'blog_post#index', as: 'blog_posts'
   get 'blog_posts/new' => 'blog_post#new', as: 'new_blog_post'
   get 'blog_posts/:id/edit' => 'blog_post#edit', as: 'edit_blog'
-  post 'blog_posts' => 'blog_post#create'
   get 'blog_posts/:id' => 'blog_post#show', as: 'blog_post'
+  post 'blog_posts' => 'blog_post#create'
+  delete 'blog_posts/;id' => 'blog_post#destroy'
+  patch 'blog_posts/:id' => 'blog_post#update'
+  
   root 'blog_post#index'
+
 
 
 end
