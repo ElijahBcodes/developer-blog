fe3dd643eee46e34aeb56b20a8dc609a7c3740de
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-01-03 18:59:19 -0800

    Delete functionality complete

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -13,10 +13,20 @@ class BlogPostController < ApplicationController
 
     def create 
         @blog_post = BlogPost.create(blog_post_params)
+        if @blog_post.valid?
+            redirect_to @blog_posts_path
+        else 
+            redirect_to new_blog_post_path
+    end
+end
+    
+
+    def destroy 
+        @blog_post = BlogPost.find(params[:id])
+        @blog_post.destroy
+            redirect_to blog_posts_path
     end
 
-    private 
-    def blog_post_params
-        params.require(:blog_post).permit(:title, :content)
-    end
+
+
 end
