99055a2c7560b4200983b139ff39bbc7d193d415
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-01-08 17:01:40 -0800

    Controller for render index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
- 
+  get 'render/index'
+ # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   get 'blog_posts' => 'blog_post#index', as: 'blog_posts'
   get 'blog_posts/new' => 'blog_post#new', as: 'new_blog_post'
   get 'blog_posts/:id/edit' => 'blog_post#edit', as: 'edit_blog'
@@ -8,7 +14,7 @@ Rails.application.routes.draw do
   delete 'blog_posts/:id' => 'blog_post#destroy', as: 'delete_blog'
   patch 'blog_posts/:id' => 'blog_post#update'
   
-  root 'blog_post#index'
+  root "render#index"
 
 
 
